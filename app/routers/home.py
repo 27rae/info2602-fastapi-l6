c2b9fecf9388054bfeb6bb8285545a2f41cd606f
27rae <rheannea.chanansingh2005@gmail.com>
2026-03-02 11:24:04 -0400

    Lab 6 completed

--- app/routers/home.py
+++ app/routers/home.py
@@ -32,3 +32,16 @@ async def app_dashboard(
             "current_user": user
         }
     )
+
+@home_router.get("/", response_class=HTMLResponse)
+async def index(
+    request: Request,
+    user_logged_in: IsUserLoggedIn,
+    db:SessionDep
+):
+    if user_logged_in:
+        user = await get_current_user(request, db)
+        if await is_admin(user):
+            return RedirectResponse(url="/admin", status_code=status.HTTP_303_SEE_OTHER)
+        return RedirectResponse(url="/app", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
